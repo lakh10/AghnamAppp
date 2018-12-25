56a0ab66d310a62b84e13087c653937926b03dc3
Adwin <dev_AccLive@users.noreply.github.com>
2018-12-25 12:39:21 +0100

    Session Persistance v.01

--- app/src/main/java/com/nibrasco/freshksa/Activities/MainActivity.java
+++ app/src/main/java/com/nibrasco/freshksa/Activities/MainActivity.java
@@ -1,15 +1,11 @@
-package com.nibrasco.freshksa;
+package com.nibrasco.freshksa.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.button.MaterialButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-import com.google.firebase.database.*;
-import com.nibrasco.freshksa.Model.Cart;
+import com.nibrasco.freshksa.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(com.nibrasco.freshksa.R.layout.activity_main);
         if (!isTaskRoot()
                 && getIntent().hasCategory(Intent.CATEGORY_LAUNCHER)
                 && getIntent().getAction() != null
@@ -26,21 +22,21 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        btnSignIn = (MaterialButton)findViewById(R.id.btnMainSignIn);
+        btnSignIn = (MaterialButton)findViewById(com.nibrasco.freshksa.R.id.btnMainSignIn);
         btnSignUp = (MaterialButton)findViewById(R.id.btnMainSignUp);
 //
 //
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signIn = new Intent(MainActivity.this, SignIn.class);
+                Intent signIn = new Intent(MainActivity.this, SignInActivity.class);
                 startActivity(signIn);
             }
         });
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signUp = new Intent(MainActivity.this, SignUp.class);
+                Intent signUp = new Intent(MainActivity.this, SignUpAcitivity.class);
                 startActivity(signUp);
             }
         });
